cf95e7cee37a42d5efedeec327361c4bc3201bcd
KiranvathanK <kirankvathan@gmail.com>
2021-07-31 11:47:04 +0530

    var and Datatypes

---
+++ var let const & DataTypes/varDataTypes.js
@@ -0,0 +1,25 @@
+// "" inside quotes writen is string datatype 
+var fullName = "kiran vathan";
+
+// var let const are used for save some memory store the data
+// hear number datatype used 
+var age = 25;
+
+//boolean datatype(true or false values for boolean)
+var isLoggedIn = true;
+
+//undefined datatype
+var paymentMode;
+//hear we dont know about incoming datatpye but we r used to store some memory if consoled get undefined 
+//bcz hear intailized but not assigned any values(output:undefined)
+
+paymentMode = "credit card";
+console.log(paymentMode);
+//hear we get output as credit card and
+ 
+//total datypes are
+//Number
+//String
+//Boolean
+//undefined
+//null
