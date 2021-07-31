8751c18665f899d4c654be4b23c1a6d32f1e62f8
KiranvathanK <kirankvathan@gmail.com>
2021-07-31 13:46:49 +0530

    typeof operator and Oprator precidence link

---
+++ Type_and_Oprator_precidence/typeOpraterPrecidence.js
@@ -0,0 +1,23 @@
+//if some thing written to get know which typeof data so for that we use 
+//typeof oprator
+
+var age = 25;
+var fullName = "kiran";
+var isLoggedIn = true;
+
+//toget data types of above we use just typeof oprators
+
+console.log(typeof age);
+console.log(typeof fullName);
+console.log(typeof isLoggedIn);
+
+//output is belove shown
+// number
+// string
+// boolean
+
+//the following link documentation for oparator precidence(ehich is first in mathematical oprators - * +() so on)
+
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Operator_Precedence
+
+//watch all precidence and much more in above website documentation link
