5c5222917d8723f6ff5256a7b5b6d2f5569cb6a5
KiranvathanK <kirankvathan@gmail.com>
2021-08-02 11:36:40 +0530

    if else conditionals

---
+++ Adding_basic_complexity_to_code2/conditionals/conditional_ifElse.js
@@ -0,0 +1,20 @@
+//for example we have to show some text to user based on temprature
+
+var temprature;
+
+//hear we are hardcoded mean giving values manullay
+
+temprature = 20;
+
+//hear we give some conditions  
+
+if (temprature < 20) {
+    console.log("cold");
+}//if conditon true its excute r else not its wont excute
+
+//if else condition 
+if (temprature < 29){      //hear its true excute
+    console.log("modrate");
+}else {                    //if else not true then this block excutes
+    console.log("hot");
+}
