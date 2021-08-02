f06ee321ea5f515885a76e179e3f3418b5c1445d
KiranvathanK <kirankvathan@gmail.com>
2021-08-02 12:56:23 +0530

    logical for allow purches example

---
+++ Adding_basic_complexity_to_code2/logical_condtn_for_purcahse/Logic.js
@@ -0,0 +1,30 @@
+//user is only allowed to purchse when he is:
+//LoggedIn
+//email varified
+//card Info
+//if any one miss above condition dont sallow to purchase
+
+isLoggedIn = true;
+emailVarified = true;
+cardInfo = true;
+
+// if (isLoggedIn){
+//     console.log("loggedin");
+//     if(emailVarified){
+//         console.log("email is varified"); 
+//         if(cardInfo){
+//             console.log("card Added");
+//         }
+//     }
+// }
+ 
+//its check all conditions one by one if login emailvarified,cardInfo then all true we have to allow to purchase
+
+//the above code is little hard so insted of that we user another type
+
+if (isLoggedIn && emailVarified && cardInfo){
+    console.log("allow to purchase");
+}
+
+//&& we used all condition must be a true if one is wrong entire block doesn't get excute
+//|| if we used or hear any one condition satisfied its excute
